c9a02a6b1f55e3690f6d4442a083995c0feb4117
neeloo patel <nilusingh945164@gmail.com>
2026-07-20 19:25:26 +0530

    tic

--- Strategies/WinningStrategies/WinningStrategy.java
+++ Strategies/WinningStrategies/WinningStrategy.java
@@ -4,6 +4,7 @@ import Module_3_LLD.TicTocToe.Model.Board;
 import Module_3_LLD.TicTocToe.Model.Move;
 
 public interface WinningStrategy {
+
     public  boolean checkWinner(Board board , Move move);
 
     void handleUndo(Move lastMove);
