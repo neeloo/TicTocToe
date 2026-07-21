de052c861599071cd0ed494a1f2ef3593b0e5fbb
neeloo patel <nilusingh945164@gmail.com>
2026-07-21 19:07:08 +0530

    tic

--- Strategies/WinningStrategies/ColWinningStrategy.java
+++ Strategies/WinningStrategies/ColWinningStrategy.java
@@ -4,6 +4,7 @@ import Module_3_LLD.TicTocToe.Model.Board;
 import Module_3_LLD.TicTocToe.Model.Move;
 
 public class ColWinningStrategy implements WinningStrategy {
+
     @Override
     public boolean checkWinner(Board board, Move move) {
         return false;
