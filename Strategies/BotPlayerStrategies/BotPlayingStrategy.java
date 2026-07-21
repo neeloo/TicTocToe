0bb349c76377f7ff794d63a3dfc4e3e387712b00
neeloo patel <nilusingh945164@gmail.com>
2026-07-21 19:10:30 +0530

    tic

--- Strategies/BotPlayerStrategies/BotPlayingStrategy.java
+++ Strategies/BotPlayerStrategies/BotPlayingStrategy.java
@@ -5,5 +5,6 @@ import Module_3_LLD.TicTocToe.Model.BotPlayer;
 import Module_3_LLD.TicTocToe.Model.Move;
 
 public interface BotPlayingStrategy  {
+
     public Move makeMove(Board board, BotPlayer botPlayer) ;
 }
